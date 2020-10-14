d91e8be1ab34a9ec5a8909060b3254c1eb94976a
Ankush Ojha <niceankushojha@gmail.com>
2020-10-14 08:19:50 +0530

    add logo in navigation

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,11 @@
 import Link from 'next/link';
 import clsx from 'clsx';
 import styles from  '../assets/css/styles.module.css';
+import logoYra from '../public/yralogo.png';
 const Navbar = () => (
   <nav className= {clsx(styles.navbar_custom, "navbar navbar-expand navbar-dark bg-dark position-relative")}>
     <div className="container-fluid">
-      <Link href="/"><a className="navbar-brand">YRA Solution</a></Link>
+      <Link href="/"><a className="navbar-brand"><img src={logoYra} className="img-fluid" alt="logo" /></a></Link>
       <div className={styles.conatct}>
           <p className="mb-0 text-white small">
           <span><a href="tel:65-81659995">+65 81659995</a></span>
