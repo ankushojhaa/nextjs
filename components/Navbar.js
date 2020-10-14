acfb17b18f3b99f7d9274cf2b6f89ed68f8257cd
Ankush Ojha <niceankushojha@gmail.com>
2020-10-14 08:52:40 +0530

    change logo path

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ import logoYra from '../public/yralogo.png';
 const Navbar = () => (
   <nav className= {clsx(styles.navbar_custom, "navbar navbar-expand navbar-dark bg-dark position-relative")}>
     <div className="container-fluid">
-      <Link href="/"><a className="navbar-brand"><img src={logoYra} className="img-fluid" alt="logo" /></a></Link>
+      <Link href="/"><a className="navbar-brand"><img src="/yralogo.png" className="img-fluid" alt="yra-logo" /></a></Link>
       <div className={styles.conatct}>
           <p className="mb-0 text-white small">
           <span><a href="tel:65-81659995">+65 81659995</a></span>
